1518754a3783d301e3cb273c2b129cf9da5103a7
Anton Harbers <Anton.Harbers23@gmail.com>
2023-11-14 14:23:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# The Odin Project - Memory Card Game
+
+A memory card game for the odin project curriculum using react.
+
+## Notes:
+
+### To-dos:
+
+```
+
+```
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>The Odin Project - Memory Game</h1>
+    </>
+  );
+}
+
+export default App;
