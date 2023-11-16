7c8770d6d3518e71c3db6e2070ea6bf6e66a0d92
Anton Harbers <Anton.Harbers23@gmail.com>
2023-11-16 21:31:58 +0100

    Finished Game Loop: On win or loss display endgame screen with options to go back to menu or try again

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ A memory card game for the odin project curriculum using react.
 ### To-dos:
 
 ```
+    Styling:
+        Basic layout and make things look kind of retro with portals maybe? (decide on a theme)
+        Animations (cards get sucked into the portals and spit out again)
+        Animate buttons and scene transitions
 
 ```
--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,48 @@ function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(getHighscoreFromStorage());
   const [clickedGifs, setClickedGifs] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [wonGame, setWonGame] = useState(false);
 
   const handleClick = (gif) => {
+    schuffleGifs();
+
     if (clickedGifs.includes(gif.id)) {
+      setGameOver(true);
       setScore(0);
       setClickedGifs([]);
+      setWonGame(false);
     } else {
       setScore(score + 1);
       setHighScore(score + 1 > highScore ? score + 1 : highScore);
       setClickedGifs([...clickedGifs, gif.id]);
+
+      if (clickedGifs.length === gifs.length - 1) {
+        setGameOver(true);
+        setWonGame(true);
+        setScore(0);
+        setClickedGifs([]);
+        return;
+      }
     }
-    schuffleGifs();
+  };
+
+  const handleBackToMenu = () => {
+    setGifs([]);
+    setGameStarted(false);
+    setGameOver(false);
   };
 
   const schuffleGifs = () => {
     const shuffledGifs = gifs.sort(() => Math.random() - 0.5);
     setGifs(shuffledGifs);
+  };
+
+  const handleReplayGame = () => {
+    setClickedGifs([]);
+    setGameStarted(true);
+    setGameOver(false);
+    setWonGame(false);
   };
 
   useEffect(() => {
@@ -73,6 +99,15 @@ function App() {
           highScore={highScore}
           setHighScore={setHighScore}
         />
+      )}
+      {gameOver && (
+        <div className="gameOverContainer">
+          {wonGame ? <h1>You won the game!</h1> : <h1>Game Over! You lost!</h1>}
+          <button onClick={handleReplayGame}>
+            {wonGame ? 'Play Again' : 'Try again'}
+          </button>
+          <button onClick={handleBackToMenu}>Back to Menu</button>
+        </div>
       )}
     </div>
   );
